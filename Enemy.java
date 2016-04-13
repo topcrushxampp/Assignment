7cf6bc84149950b75a63422234103057e8ee9da5
topcrushxampp <toffie.z@hotmail.com>
2016-04-13 17:31:18 +0700

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -1,6 +1,12 @@
-package f2.spw;             #CP02
+package f2.spw;            // #CP02
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.AlphaComposite;
 
 public class Enemy extends Sprite{
+	public static final int Y_TO_FADE = 400;
+	public static final int Y_TO_DIE = 600;
+	
 	private int step = 12;
 	private boolean alive = true;
 	
@@ -8,8 +14,19 @@ public class Enemy extends Sprite{
 		super(x, y,15, 5);
 		
 	}
-
-	public void proceed(){
+	@Override
+	public void draw(Graphics2D g) {
+		if(y < Y_TO_FADE)
+			g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f));
+		else{
+			g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 
+					(float)(Y_TO_DIE - y)/(Y_TO_DIE - Y_TO_FADE)));
+		}
+		g.setColor(Color.RED);
+		g.fillRect(x, y, width, height);
+		
+	}
+	public void proceed(){    // CP#03
 		y += step;
 		if(y > Y_TO_DIE){
 			alive = false;
